6352008ba84b3104612d2b29e3e91f3c180f17a1
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-09-16 01:59:03 +0000

    Update CORS settings

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -61,14 +61,15 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
-    os.environ.get('ALLOWED_HOST'),
-    'localhost',
-    '8000-thomastomo-drfapi-gjbxnyd08k0.ws-eu104.gitpod.io',
-    'https://drf-api-thomas-48cbbb8b8839.herokuapp.com/'
+    os.environ.get("ALLOWED_HOST"),
+    "localhost",
 ]
-
-if 'CLIENT_ORIGIN_DEV' in os.environ:
-    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+if "CLIENT_ORIGIN" in os.environ:
+    CORS_ALLOWED_ORIGINS = [os.environ.get("CLIENT_ORIGIN")]
+if "CLIENT_ORIGIN_DEV" in os.environ:
+    extracted_url = re.match(
+        r"^.+-", os.environ.get("CLIENT_ORIGIN_DEV", ""), re.IGNORECASE
+    ).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
         rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
